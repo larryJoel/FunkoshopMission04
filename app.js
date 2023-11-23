b0aa11b97b0861149c892546cf51f9d7aa270f8d
Larry González <larrygonzalez@icsred.com>
2023-11-23 07:54:17 -0300

    My first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.get('/home',(req, res)=> res.sendFile(__dirname + '/public/index.html'));
+app.get('/home',(req, res)=> res.sendFile(__dirname + '/public/index.html'));//ruta dinámica
 app.get('/ping', (req, res)=> res.send('pong'));
 
 app.listen(4000, ()=> console.log('servidor corriendo en Http://localhost:4000'));
