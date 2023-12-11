ec2a1cda6f59ff39470a1bda926e9c30103ebd8b
larryjoel <larry.g.m1024@gmail.com>
2023-12-11 20:19:57 -0300

    My first commit

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express =  require('express');
+const app = express();
+const path = require('path');
+
+/*Puerto de la aplicación */
+const PORT = 4000;
 const mainRoutes = require('./src/routes/main.routes.js');
 const shopRoutes = require('./src/routes/shop.routes.js');
 const admRoutes = require('./src/routes/admin.routes.js');
 const authRoutes = require('./src/routes/auth.routes.js');
-const app = express();
 
-//middleware
+//Parsea los datos recibidos por POST
 app.use(express.static('public'));
+app.use(express.json());
+
+/*Middleware a las rutas */
 app.use('/', mainRoutes);
 app.use('/shop',shopRoutes);
 app.use('/auth/admin', authRoutes);
 app.use('/adm', admRoutes);
 
 
-app.listen(4000, ()=> console.log('servidor corriendo en Http://localhost:4000'));
+app.use((req, res, next) => {
+    const filePath = path.join(__dirname, './public/pages/404.html');
+    res.status(404).sendFile(filePath);
+});
+
+app.listen(PORT, ()=> console.log('servidor corriendo en Http://localhost:'+PORT));
